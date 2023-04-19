847822b0163f9b584039b34f4ea7c8ca1d152107
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-04-19 16:27:18 +0300

    Initial commit

--- src/Draft_1_Case_3_1_1.java
+++ src/Draft_1_Case_3_1_1.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Draft_1_Case_3_1_1 {
+public class Draft_1_Case_3_1_1 {
+//Здесь Java. Поиск второго по минимальности элемента в массиве. https://youtu.be/03iETRvZrFg
+    public static void main(String[] args) {
+
+    }
 }
